843ebfba4150a26981264df622a6c028a06b1a91
FIvanO <FIvanO1998@gmail.com>
2018-07-04 14:59:31 +0300

    create main class

--- dog.cpp
+++ dog.cpp
@@ -1,6 +1,24 @@
 #include "dog.h"
 
-Dog::Dog()
+Dog::Dog(IRunStrategy *_rs, IWuffStrategy *_ws, IDisplayStrategy *_ds) : rs(_rs), ws(_ws), ds(_ds)
 {
-
 }
+
+void Dog::run() {
+	rs->run();
+}
+
+void Dog::wuff() {
+	ws->wuff();
+}
+
+void Dog::display() {
+	ds->display();
+}
+
+Dog::~Dog() {
+	delete rs;
+	delete ws;
+	delete ds;
+}
+
--- dog.h
+++ dog.h
@@ -1,11 +1,22 @@
 #ifndef DOG_H
 #define DOG_H
 
+#include "irunstrategy.h"
+#include "iwuffstrategy.h"
+#include "idisplaystrategy.h"
 
 class Dog
 {
 public:
-	Dog();
+	Dog(IRunStrategy *_rs, IWuffStrategy *_ws, IDisplayStrategy *_ds);
+	void run();
+	void wuff();
+	void display();
+	~Dog();
+private:
+	IRunStrategy *rs;
+	IWuffStrategy *ws;
+	IDisplayStrategy *ds;
 };
 
 #endif // DOG_H
