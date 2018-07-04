998b38e249e5a5f9813c73ac83f7870e223f24ed
FIvanO <FIvanO1998@gmail.com>
2018-07-04 15:05:59 +0300

    main

--- main.cpp
+++ main.cpp
@@ -1,8 +1,31 @@
-#include <QCoreApplication>
+#include <iostream>
+#include "dog.h"
+#include "homerunstrategy.h"
+#include "norunstrategy.h"
+#include "baserunstrategy.h"
+#include "rubberwuffstrategy.h"
+#include "wildwuffstrategy.h"
+#include "basewuffstrategy.h"
+#include "rubberdisplaystrategy.h"
+#include "wilddisplaystrategy.h"
+#include "citydisplaystrategy.h"
+#include "homedisplaystrategy.h"
+#include <vector>
 
-int main(int argc, char *argv[])
+int main()
 {
-	QCoreApplication a(argc, argv);
+	std::vector <Dog> dogs;
+	dogs.push_back(Dog(new HomeRunStrategy(), new BaseWuffStrategy(), new HomeDisplayStrategy())); // homeDog
+	dogs.push_back(Dog(new BaseRunStrategy(), new BaseWuffStrategy(), new CityDisplayStrategy())); // cityDog
+	dogs.push_back(Dog(new BaseRunStrategy(), new WildWuffStrategy(), new WildDisplayStrategy())); // wildDog
+	dogs.push_back(Dog(new NoRunStrategy(), new RubberWuffStrategy(), new RubberDisplayStrategy())); // rubberDog
 
-	return a.exec();
+	for (auto it = dogs.begin(); it != dogs.end(); ++it) {
+		it->run();
+		it->wuff();
+		it->display();
+	}
+
+
+	return 0;
 }
