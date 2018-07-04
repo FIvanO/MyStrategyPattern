65aa2f38c483362d6ebdd309922ce12a662a27a0
FIvanO <FIvanO1998@gmail.com>
2018-07-04 14:56:07 +0300

    Interface for display strategy

--- idisplaystrategy.h
+++ idisplaystrategy.h
@@ -1,4 +1,12 @@
 #ifndef IDISPLAYSTRATEGY_H
 #define IDISPLAYSTRATEGY_H
 
+class IDisplayStrategy
+{
+public:
+	IDisplayStrategy() {}
+	virtual void display() {}
+	virtual ~IDisplayStrategy() {}
+};
+
 #endif // IDISPLAYSTRATEGY_H
