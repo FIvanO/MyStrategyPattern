19c41ea671696d3711e89727a2dd1266c79e503e
FIvanO <FIvanO1998@gmail.com>
2018-07-04 14:56:40 +0300

    Interface for wuff strategy

--- iwuffstrategy.h
+++ iwuffstrategy.h
@@ -1,4 +1,12 @@
 #ifndef IWUFFSTRATEGY_H
 #define IWUFFSTRATEGY_H
 
+class IWuffStrategy
+{
+public:
+	IWuffStrategy() {}
+	virtual void wuff() {}
+	virtual ~IWuffStrategy() {}
+};
+
 #endif // IWUFFSTRATEGY_H
