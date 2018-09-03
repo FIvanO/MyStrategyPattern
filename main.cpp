fed5f22e3db75a60aa608f05479e90ddf29ecf30
FIvanO <FIvanO1998@gmail.com>
2018-09-03 17:24:00 +0300

    make random dogs

--- main.cpp
+++ main.cpp
@@ -10,8 +10,10 @@
 #include "wilddisplaystrategy.h"
 #include "citydisplaystrategy.h"
 #include "homedisplaystrategy.h"
+#include "enums.h"
 #include <vector>
 #include <memory>
+#include <ctime>
 
 typedef std::shared_ptr<IDisplayStrategy> SharedPtrDis;
 typedef std::shared_ptr<IRunStrategy>     SharedPtrRun;
@@ -20,42 +22,22 @@ typedef std::shared_ptr<IWuffStrategy>    SharedPtrWuff;
 int main()
 {
 
-    enum class dis_type {
-        home = 0,
-        city = 1,
-        wild = 2,
-        rubb = 3
-    };
-
     std::vector<SharedPtrDis> display_strategies;
     display_strategies.push_back(SharedPtrDis(new HomeDisplayStrategy()));
     display_strategies.push_back(SharedPtrDis(new CityDisplayStrategy()));
     display_strategies.push_back(SharedPtrDis(new WildDisplayStrategy()));
     display_strategies.push_back(SharedPtrDis(new RubberDisplayStrategy()));
-//----------------------------------------------------
-
-    enum class run_type {
-        home = 0,
-        base = 1,
-        no   = 2
-    };
 
     std::vector<SharedPtrRun> run_strategies;
     run_strategies.push_back(SharedPtrRun(new HomeRunStrategy()));
     run_strategies.push_back(SharedPtrRun(new BaseRunStrategy()));
     run_strategies.push_back(SharedPtrRun(new NoRunStrategy()));
-//----------------------------------------------------
-
-    enum class wuff_type {
-        base = 0,
-        wild = 1,
-        rubb = 2
-    };
 
     std::vector<SharedPtrWuff> wuff_strategies;
     wuff_strategies.push_back(SharedPtrWuff(new BaseWuffStrategy()));
     wuff_strategies.push_back(SharedPtrWuff(new WildWuffStrategy()));
     wuff_strategies.push_back(SharedPtrWuff(new RubberWuffStrategy()));
+
 //----------------------------------------------------
 
     std::vector<Dog> dogs;
@@ -90,7 +72,26 @@ int main()
             display_strategies[static_cast<size_t>(dis_type::rubb)]
             )
      ); // rubberDog
+
+
+    srand(time(nullptr));
+    dogs.push_back( // randomDog 1
+        Dog(
+            run_strategies    [static_cast<size_t>(rand() % 3)],
+            wuff_strategies   [static_cast<size_t>(rand() % 3)],
+            display_strategies[static_cast<size_t>(rand() % 4)]
+            )
+     ); // randomDog 1
+
+    dogs.push_back( // randomDog 2
+        Dog(
+            run_strategies    [static_cast<size_t>(rand() % 3)],
+            wuff_strategies   [static_cast<size_t>(rand() % 3)],
+            display_strategies[static_cast<size_t>(rand() % 4)]
+            )
+     ); // randomDog 2
 //----------------------------------------------------
+
 
 	for (auto it = dogs.begin(); it != dogs.end(); ++it) {
         std::cout << "New dog: \n";
@@ -101,7 +102,8 @@ int main()
         std::cout << "  ";
         it->display();
         std::cout << "End dog \n\n";
-	}
+        std::cout << "\n";
+    }
 
 
     std::cout << "\n\n";
